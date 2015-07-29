507c829b87901575bd922f599586ea1024c6520b
Maxime Perrotin <maxime.perrotin@esa.int>
2015-07-29 15:39:59 +0200

    Update XML backend

--- Backend2/XmlAst.fs
+++ Backend2/XmlAst.fs
@@ -34,19 +34,19 @@ let rec PrintType (t:Asn1Type) modName (r:AstRoot) =
         | NullType              -> xml.NullType ()
         | Choice(children)      -> 
             let emitChild (c:ChildInfo) =
-                xml.ChoiceChild c.Name.Value (BigInteger c.Name.Location.srcLine) (BigInteger c.Name.Location.charPos) (PrintType c.Type modName r) (c.CName_Present C)
+                xml.ChoiceChild c.Name.Value (ToC c.Name.Value) (BigInteger c.Name.Location.srcLine) (BigInteger c.Name.Location.charPos) (PrintType c.Type modName r) (c.CName_Present C)
             xml.ChoiceType (children |> Seq.map emitChild)
         | Sequence(children)    -> 
             let emitChild (c:ChildInfo) =
                 match c.Optionality with
-                | Some(Default(vl)) -> xml.SequenceChild c.Name.Value true (PrintAsn1.PrintAsn1Value vl) (BigInteger c.Name.Location.srcLine) (BigInteger c.Name.Location.charPos) (PrintType c.Type modName r)
-                | _                 -> xml.SequenceChild c.Name.Value c.Optionality.IsSome null (BigInteger c.Name.Location.srcLine) (BigInteger c.Name.Location.charPos) (PrintType c.Type modName r)
+                | Some(Default(vl)) -> xml.SequenceChild c.Name.Value (ToC c.Name.Value) true (PrintAsn1.PrintAsn1Value vl) (BigInteger c.Name.Location.srcLine) (BigInteger c.Name.Location.charPos) (PrintType c.Type modName r)
+                | _                 -> xml.SequenceChild c.Name.Value (ToC c.Name.Value) c.Optionality.IsSome null (BigInteger c.Name.Location.srcLine) (BigInteger c.Name.Location.charPos) (PrintType c.Type modName r)
             xml.SequenceType (children |> Seq.map emitChild)
         | Enumerated(items)     -> 
             let emitItem (idx: int) (it : Ast.NamedItem) =
                 match it._value with
-                | Some(vl)  -> xml.EnumItem it.Name.Value (Ast.GetValueAsInt vl r) (BigInteger it.Name.Location.srcLine) (BigInteger it.Name.Location.charPos) (it.CEnumName r C)
-                | None      -> xml.EnumItem it.Name.Value (BigInteger idx) (BigInteger it.Name.Location.srcLine) (BigInteger it.Name.Location.charPos) (it.CEnumName r C)
+                | Some(vl)  -> xml.EnumItem it.Name.Value (ToC it.Name.Value) (Ast.GetValueAsInt vl r) (BigInteger it.Name.Location.srcLine) (BigInteger it.Name.Location.charPos) (it.CEnumName r C)
+                | None      -> xml.EnumItem it.Name.Value (ToC it.Name.Value) (BigInteger idx) (BigInteger it.Name.Location.srcLine) (BigInteger it.Name.Location.charPos) (it.CEnumName r C)
             xml.EnumType (items |> Seq.mapi emitItem)
         | SequenceOf(child)     -> 
             let sMin, sMax = GetMinMax (GetTypeUperRange t.Kind t.Constraints r) 
@@ -59,8 +59,8 @@ let rec PrintType (t:Asn1Type) modName (r:AstRoot) =
                 | Boolean | NullType | Choice(_) | Enumerated(_) | Sequence(_) | ReferenceType(_)       -> None
             let sModName = if md.Value=modName then null else md.Value
             match uperRange with
-            | Some(sMin, sMax)  -> xml.RefTypeMinMax sMin sMax ts.Value sModName
-            | None              -> xml.RefType ts.Value sModName
+            | Some(sMin, sMax)  -> xml.RefTypeMinMax sMin sMax ts.Value sModName (ToC ts.Value) (ToC sModName)
+            | None              -> xml.RefType ts.Value sModName (ToC ts.Value) (ToC sModName)
 
     xml.TypeGeneric (BigInteger t.Location.srcLine) (BigInteger t.Location.charPos) (PrintTypeAux t)
 
@@ -71,13 +71,13 @@ let DoWork (r:AstRoot) =
         | All -> true
         | _ -> false        
     let PrintVas (vas: Ast.ValueAssignment) modName =
-        xml.VasXml vas.Name.Value (BigInteger vas.Name.Location.srcLine) (BigInteger vas.Name.Location.charPos) (PrintType vas.Type modName r) (PrintAsn1.PrintAsn1Value vas.Value)
+        xml.VasXml vas.Name.Value (BigInteger vas.Name.Location.srcLine) (BigInteger vas.Name.Location.charPos) (PrintType vas.Type modName r) (PrintAsn1.PrintAsn1Value vas.Value) (ToC vas.Name.Value)
     let PrintTas (tas:Ast.TypeAssignment) modName =
-        xml.TasXml tas.Name.Value (BigInteger tas.Name.Location.srcLine) (BigInteger tas.Name.Location.charPos) (PrintType tas.Type modName r)
+        xml.TasXml tas.Name.Value (BigInteger tas.Name.Location.srcLine) (BigInteger tas.Name.Location.charPos) (PrintType tas.Type modName r) (ToC tas.Name.Value) (xml.AssigOpNormalType ())
     let PrintModule (m:Asn1Module) =
         let PrintImpModule (im:Ast.ImportedModule) =
-            xml.ImportedMod im.Name.Value (im.Types |> Seq.map(fun x -> x.Value)) (im.Values |> Seq.map(fun x -> x.Value))
-        xml.ModuleXml m.Name.Value (m.Imports |> Seq.map PrintImpModule) m.ExportedTypes m.ExportedVars (m.TypeAssignments |> Seq.map (fun t -> PrintTas t m.Name.Value)) (m.ValueAssignments |> Seq.map (fun t -> PrintVas t m.Name.Value)) (AllExported m)
+            xml.ImportedMod im.Name.Value (ToC im.Name.Value) (im.Types |> Seq.map(fun x -> x.Value)) (im.Values |> Seq.map(fun x -> x.Value))
+        xml.ModuleXml m.Name.Value (ToC m.Name.Value) (m.Imports |> Seq.map PrintImpModule) m.ExportedTypes m.ExportedVars (m.TypeAssignments |> Seq.map (fun t -> PrintTas t m.Name.Value)) (m.ValueAssignments |> Seq.map (fun t -> PrintVas t m.Name.Value)) (AllExported m)
     let PrintFile (f:Asn1File) =
         xml.FileXml f.FileName (f.Modules |> Seq.map PrintModule)
     let content = xml.RootXml (r.Files |> Seq.map PrintFile)
