ad9c4eded2a8eebc9a74b419f865abaa93e26091
gmamais <gmamais@gmail.com>
2015-09-26 12:18:22 +0300

    introducing catamorphismes (generalized fold)

---
+++ Ast/GenericFold.fs
@@ -0,0 +1,388 @@
+﻿module GenericFold
+open Ast
+open Monads
+open FsUtils
+
+let enumIntegerType = 
+    {
+        Asn1Type.Kind = Integer
+        Constraints = []
+        AcnProperties = []
+        Location = FsUtils.emptyLocation
+    }
+
+let stringType = 
+    {
+        Asn1Type.Kind = IA5String
+        Constraints = []
+        AcnProperties = []
+        Location = FsUtils.emptyLocation
+    }
+
+let sizeIntegerType = 
+    {
+        Asn1Type.Kind = Integer
+        Constraints = [RangeContraint_val_MAX ({Asn1Value.Kind = IntegerValue(FsUtils.IntLoc.ByValue 0I); Location=FsUtils.emptyLocation},true)]
+        AcnProperties = []
+        Location = FsUtils.emptyLocation
+    }
+
+
+let error (loc: SrcLoc) format = 
+    let doAfter s = 
+        raise (SemanticError (loc, s))
+    Printf.ksprintf doAfter format 
+
+type CheckContext =
+    | CheckContent
+    | CheckSize          
+    | CheckCharacter    
+
+
+let foldAstRoot 
+    roorFunc                    
+    fileFunc        
+    modFunc 
+    tasFunc 
+    vasFunc 
+    refTypeFunc 
+    baseTypeFunc 
+    enmItemFunc 
+    seqOfTypeFunc 
+    seqTypeFunc 
+    chTypeFunc 
+    sequenceChildFunc 
+    choiceChildFunc 
+    refValueFunc  
+    enumValueFunc 
+    intValFunc 
+    realValFunc 
+    ia5StringValFunc 
+    numStringValFunc 
+    boolValFunc 
+    octetStringValueFunc 
+    bitStringValueFunc 
+    nullValueFunc 
+    seqOfValueFunc 
+    seqValueFunc 
+    chValueFunc
+    singleValueContraintFunc 
+    rangeContraintFunc 
+    greaterThanContraintFunc
+    lessThanContraintFunc
+    alwaysTtrueContraintFunc
+    typeInclConstraintFunc
+    unionConstraintFunc
+    intersectionConstraintFunc
+    notConstraintFunc
+    exceptConstraintFunc
+    rootConstraintFunc
+    root2ConstraintFunc
+    (r:AstRoot) =
+
+    let rec loopAstRoot (r:AstRoot) =
+        cont {
+            let! files = r.Files |> mmap  (loopFile r)
+            return roorFunc r files
+        }
+    and loopFile (r:AstRoot) (f:Asn1File) =
+        cont {
+            let! modules = f.Modules |> mmap  (loopModule r f)
+            return fileFunc r f modules
+        }
+    and loopModule (r:AstRoot) (f:Asn1File) (m:Asn1Module) =
+        cont {
+            let! tases = m.TypeAssignments |> mmap  (loopTypeAssignment r f m)
+            let! vases = m.ValueAssignments |> mmap  (loopValueAssignments r f m)
+            return modFunc r f m tases vases
+        }
+    and loopTypeAssignment (r:AstRoot) (f:Asn1File) (m:Asn1Module) (tas:TypeAssignment) =
+        cont {
+            let! asn1Type = loopType r f m tas.Type
+            return tasFunc r f m tas asn1Type
+        }
+    and loopValueAssignments (r:AstRoot) (f:Asn1File) (m:Asn1Module) (vas:ValueAssignment) =
+        cont {
+            let actType = GetActualType vas.Type  r
+            let! asn1Type = loopType r f m vas.Type
+            let! asn1Value = loopAsn1Value r f m actType vas.Value
+            return vasFunc r f m vas asn1Type asn1Value
+        }
+    and loopType (r:AstRoot) (f:Asn1File) (m:Asn1Module) (t:Asn1Type) =
+        cont {
+            match t.Kind with
+            | ReferenceType (mdName,tasName, argList) ->
+                let eqType = GetActualType t r 
+                let! refCons = t.Constraints |> mmap  (loopConstraint r f m eqType CheckContent)
+                let! newActType = loopType r f m eqType 
+                return refTypeFunc r f m mdName.Value tasName.Value newActType refCons
+            | Integer 
+            | Real    
+            | IA5String 
+            | NumericString 
+            | OctetString 
+            | NullType 
+            | BitString 
+            | Boolean           ->
+                let! newCons = t.Constraints |> mmap  (loopConstraint r f m t CheckContent)
+                return baseTypeFunc r f m t newCons            
+            | Enumerated (enmItems) ->  
+                let! newCons = t.Constraints |> mmap  (loopConstraint r f m t CheckContent)
+                let! newEnmItems = enmItems |> mmap (loopNamedItem r f m)
+                // to do : values in new named items should be resolved and always present
+                // plus a flag in the enum type whether values where provided by user or not/
+                return enmItemFunc r f m t newEnmItems newCons            
+            | SequenceOf (innerType)  ->
+                let! newCons = t.Constraints |> mmap  (loopConstraint r f m t CheckContent)
+                let! newInnerType = loopType r f m  innerType
+                return seqOfTypeFunc r f m t  newInnerType newCons
+            | Sequence (children)     ->
+                let! newCons = t.Constraints |> mmap  (loopConstraint r f m t CheckContent)
+                let! newChildren = children |> mmap  (loopSequenceChild r f m )
+                return seqTypeFunc r f m t newChildren newCons
+            | Choice (children)       ->
+                let! newCons = t.Constraints |> mmap  (loopConstraint r f m t CheckContent)
+                let! newChildren = children |> mmap  (loopChoiceChild r f m )
+                return chTypeFunc r f m t newChildren newCons
+        }
+
+   
+    and loopNamedItem (r:AstRoot) (f:Asn1File) (m:Asn1Module) (ni:NamedItem) =
+        cont {
+            match ni._value with
+            | Some v    ->
+                let! newValue = loopAsn1Value r f m enumIntegerType v
+                return (ni.Name, ni.Comments, Some newValue)
+            | None      ->
+                return (ni.Name, ni.Comments, None)
+        }
+    and loopSequenceChild (r:AstRoot) (f:Asn1File) (m:Asn1Module)  (chInfo:ChildInfo) =
+        cont {
+            let! newType = loopType r f m  chInfo.Type
+            let! newDefValue = 
+                cont {
+                    match chInfo.Optionality with
+                    | Some Asn1Optionality.AlwaysAbsent
+                    | Some Asn1Optionality.AlwaysPresent
+                    | Some Asn1Optionality.Optional          
+                    | None                                  -> 
+                        return None
+                    | Some  (Asn1Optionality.Default   vl)  ->
+                        let eqType = GetActualType chInfo.Type r
+                        let! newValue = loopAsn1Value r f m eqType vl
+                        return Some newValue
+                }
+            return sequenceChildFunc r f m chInfo newType newDefValue
+        }
+
+    and loopChoiceChild (r:AstRoot) (f:Asn1File) (m:Asn1Module) (chInfo:ChildInfo) =
+        cont {
+            let! newType = loopType r f m  chInfo.Type
+            return choiceChildFunc r f m   chInfo newType
+        }
+    and loopAsn1Value (r:AstRoot) (f:Asn1File) (m:Asn1Module) (t:Asn1Type) (v:Asn1Value) =
+        match v.Kind, t.Kind with
+        | RefValue (md,vas), Enumerated (enmItems)   ->
+                match enmItems |> Seq.tryFind (fun x -> x.Name.Value = vas.Value) with
+                | Some enmItem    ->
+                    cont {
+                        let! actType = loopType r f m t
+                        let! ni = loopNamedItem r f m enmItem
+                        return enumValueFunc r f m t actType enmItem  ni          
+                    }
+                | None          ->
+                    cont {
+                        let actValue = GetActualValue md vas r
+                        let! newActVal = loopAsn1Value r f m t actValue
+                        return refValueFunc r f m t (md.Value, vas.Value) newActVal            
+                    }
+        | RefValue (md,vas), _   ->
+                cont {
+                    let actValue = GetActualValue md vas r
+                    let! newActVal = loopAsn1Value r f m t actValue
+                    return refValueFunc r f m t (md.Value, vas.Value) newActVal            
+                }
+        | IntegerValue bi, Integer          ->
+            cont {
+                let! actType = loopType r f m t
+                return intValFunc r f m t actType bi.Value
+            }
+        | IntegerValue bi, Real  _           ->
+            cont {
+                let! actType = loopType r f m t
+                let dc:double = (double)bi.Value
+                return realValFunc r f m t actType dc
+            }
+        | RealValue dc , Real   _                  -> 
+            cont {
+                let! actType = loopType r f m t
+                return realValFunc r f m t actType dc.Value
+            }
+        | StringValue s, IA5String _   -> 
+            cont {
+                let! actType = loopType r f m t
+                return ia5StringValFunc r f m t actType s.Value
+            }
+        | StringValue s , NumericString _   -> 
+            cont {
+                let! actType = loopType r f m t
+                return numStringValFunc r f m t actType s.Value
+            }
+        | BooleanValue b, Boolean _       ->
+            cont {
+                let! actType = loopType r f m t
+                return boolValFunc r f m t actType b.Value
+            }
+        | OctetStringValue b, OctetString _         ->
+            cont {
+                let! actType = loopType r f m t
+                return octetStringValueFunc r f m t actType b
+            }
+        | OctetStringValue b, BitString _        ->
+            cont {
+                let! actType = loopType r f m t
+                return octetStringValueFunc r f m t actType b
+            }
+        | BitStringValue b, BitString _           ->
+            cont {
+                let! actType = loopType r f m t
+                return bitStringValueFunc r f m t actType b 
+            }
+        | NullValue , NullType _   ->
+            cont {
+                let! actType = loopType r f m t
+                return nullValueFunc r f m t actType             
+            }
+        | SeqOfValue  vals, SequenceOf chType    ->
+            cont {
+                let! actType = loopType r f m t
+                let eqType = GetActualType chType r
+                let! newVals = vals |> mmap  (loopAsn1Value r f m eqType)
+                return seqOfValueFunc r f m t actType  newVals
+            }
+        | SeqValue    namedValues, Sequence children ->
+            cont {
+                let! actType = loopType r f m t
+                let! newValues =  namedValues |> mmap  (loopSeqValueChild r f m  children )
+                return seqValueFunc r f m t actType newValues
+            }
+        | ChValue (name,vl), Choice children         ->
+            match children |> Seq.tryFind(fun ch -> ch.Name.Value = name.Value) with
+            | Some chType   ->
+                cont {
+                    let! actType = loopType r f m t
+                    let eqType = GetActualType chType.Type r 
+                    let! newValue = loopAsn1Value r f m eqType vl
+                    return chValueFunc r f m t actType name newValue
+                }
+            | None  -> 
+                error name.Location "Invalid alternative name '%s'" name.Value
+        | _         ->
+            error v.Location "Invalid combination ASN.1 type and ASN.1 value"
+
+
+    and loopSeqValueChild (r:AstRoot) (f:Asn1File) (m:Asn1Module)  (children: list<ChildInfo>) (nm:StringLoc, chv:Asn1Value) = 
+        cont {
+            let child = 
+                match children |> Seq.tryFind (fun ch -> ch.Name.Value = nm.Value) with
+                | Some ch -> ch
+                | None    -> error nm.Location "Invalid child name '%s'" nm.Value
+
+            let eqType = GetActualType child.Type r
+            let! newValue = loopAsn1Value r f m eqType chv
+            return ((nm,loc),newValue)
+        }
+
+    and loopConstraint (r:AstRoot) (f:Asn1File) (m:Asn1Module) (t:Asn1Type) (checContent:CheckContext) (c:Asn1Constraint) =
+        let vtype =
+            match checContent with
+            | CheckSize         -> sizeIntegerType
+            | CheckCharacter    -> stringType
+            | CheckContent      -> t                 
+
+        match c with
+        | SingleValueContraint v        ->
+            cont {
+                let! actType = loopType r f m t
+                let! newValue = loopAsn1Value r f m vtype v
+                return singleValueContraintFunc r f m t checContent actType newValue
+            }
+        | RangeContraint(v1,v2,b1,b2)   -> 
+            cont {
+                let! actType = loopType r f m t
+                let! newValue1 = loopAsn1Value r f m vtype v1
+                let! newValue2 = loopAsn1Value r f m vtype v2
+                return rangeContraintFunc r f m t checContent actType newValue1 newValue2 b1 b2
+            }
+        | RangeContraint_val_MAX (v,b)  ->
+            cont {
+                let! actType = loopType r f m t
+                let! newValue = loopAsn1Value r f m vtype v
+                return greaterThanContraintFunc r f m t checContent actType newValue  b
+            }
+        | RangeContraint_MIN_val (v,b)  ->
+            cont {
+                let! actType = loopType r f m t
+                let! newValue = loopAsn1Value r f m vtype v
+                return lessThanContraintFunc r f m t checContent actType newValue  b
+            }
+        | RangeContraint_MIN_MAX             -> 
+            cont {
+                let! actType = loopType r f m t
+                return alwaysTtrueContraintFunc r f m t checContent actType 
+            }
+        | TypeInclusionConstraint (md,tas)  ->
+            cont {
+                let! actType = loopType r f m t
+                return typeInclConstraintFunc r f m t actType (md,tas)
+            }
+        | UnionConstraint (c1,c2,v)      ->
+            cont {
+                let! actType = loopType r f m t
+                let! nc1 = loopConstraint r f m t checContent c1
+                let! nc2 = loopConstraint r f m t checContent c2
+                return unionConstraintFunc r f m t actType nc1 nc2
+            }
+        | IntersectionConstraint(c1,c2)         ->
+            cont {
+                let! actType = loopType r f m t
+                let! nc1 = loopConstraint r f m t checContent c1
+                let! nc2 = loopConstraint r f m t checContent c2
+                return intersectionConstraintFunc r f m t actType nc1 nc2
+            }
+        | AllExceptConstraint c ->
+            cont {
+                let! actType = loopType r f m t
+                let! nc = loopConstraint r f m t checContent c
+                return notConstraintFunc r f m t actType nc
+            }
+        | ExceptConstraint (c1,c2)  ->
+            cont {
+                let! actType = loopType r f m t
+                let! nc1 = loopConstraint r f m t checContent c1
+                let! nc2 = loopConstraint r f m t checContent c2
+                return exceptConstraintFunc r f m t actType nc1 nc2
+            }
+        | RootConstraint c  ->
+            cont {
+                let! actType = loopType r f m t
+                let! nc = loopConstraint r f m t checContent c
+                return rootConstraintFunc r f m t actType nc 
+            }
+        | RootConstraint2 (c1,c2)  ->
+            cont {
+                let! actType = loopType r f m t
+                let! nc1 = loopConstraint r f m t checContent c1
+                let! nc2 = loopConstraint r f m t checContent c2
+                return root2ConstraintFunc r f m t actType nc1 nc2
+            }
+
+        | SizeContraint(sc)         -> loopConstraint r f m t CheckSize sc
+
+        | AlphabetContraint c       -> loopConstraint r f m t CheckCharacter c
+
+        | WithComponentConstraint _          -> raise(BugErrorException "This constraint should have been removed")
+        | WithComponentsConstraint _         -> raise(BugErrorException "This constraint should have been removed")
+                
+
+    loopAstRoot r id 
---
+++ Backend.c/c_validate2.fs
@@ -0,0 +1,258 @@
+﻿module c_validate2
+
+open System.Numerics
+open FsUtils
+open Ast
+open System.IO
+open VisitTree
+open uPER
+open CloneTree
+open c_utils
+
+
+type TasValidateFunc = {
+    moduleName  : string // the module where this function lives. I C, this is dummy
+    functionName: string 
+    validateType: string //e.g. MyStruct*
+    typeContent : ValidatedTypeStatementBlock
+}
+with
+  member this.LocalVariables : List<LocalVariable> =
+    []
+
+and LocalVariable = {
+    varName:string
+    varType:string //e.g. int, char* etc
+}
+
+and ValidatedTypeStatementBlock =
+    | BaseTypedValidateTypeStatementBlock of BaseTypedValidateTypeStatementBlock
+    | RefTypedValidateTypeStatementsBlock of RefTypedValidateTypeStatementsBlock
+    | SeqOfTypedValidateTypeStatementsBlock of SeqOfTypedValidateTypeStatementsBlock
+    | ChoiceTypedValidateTypeStatementsBlock of ChoiceTypedValidateTypeStatementsBlock
+    | SequenceTypedValidateTypeStatementsBlock  of SequenceTypedValidateTypeStatementsBlock
+    with
+        member x.AccessPath : string =
+            match x with
+            | BaseTypedValidateTypeStatementBlock        x  -> x.accessPath
+            | RefTypedValidateTypeStatementsBlock        x  -> x.accessPath
+            | SeqOfTypedValidateTypeStatementsBlock      x  -> x.accessPath
+            | ChoiceTypedValidateTypeStatementsBlock     x  -> x.accessPath
+            | SequenceTypedValidateTypeStatementsBlock   x  -> x.accessPath
+
+and BaseTypedValidateTypeStatementBlock = {
+    accessPath : string                 //e.g. pVal->field1.field2, pVal->array1[i1]
+    returnErrorCode : int               //error code to return if validations fail
+    validatedContraintExpressions : ValidatedContraintExpression list
+}
+
+and RefTypedValidateTypeStatementsBlock = {
+    accessPath : string                 //e.g. pVal->field1.field2
+    refencedTasFunc     : (string*string) // the function to be called
+}
+
+and SeqOfTypedValidateTypeStatementsBlock = {
+    accessPath : string                         //e.g. pVal->field1.field2
+    index      : LocalVariable
+    LengthType : bool                           
+    validatedSizeContraintExpressions : ValidatedContraintExpression list
+    child : ValidatedTypeStatementBlock
+}
+
+and ChoiceTypedValidateTypeStatementsBlock = {
+    accessPath : string                         //e.g. pVal->field1.field2
+    contraintExpressions : ValidatedContraintExpression list        //usually this is empty
+    children : (string*ValidatedTypeStatementBlock) list        //present expression, child block
+}
+
+and SequenceTypedValidateTypeStatementsBlock = {
+    accessPath : string                         //e.g. pVal->field1.field2
+    contraintExpressions : ValidatedContraintExpression list        //usually this is empty
+    children : SequenceChildTypedValidateTypeStatementsBlock list
+}
+
+and SequenceChildTypedValidateTypeStatementsBlock = 
+    | MandatoryChild of ValidatedTypeStatementBlock                 //child block
+    | OptionalChild of string*ValidatedTypeStatementBlock           //child exists expression, child block
+    | AlwaysPresentChild of string*ValidatedTypeStatementBlock      //child exists expression, child block
+    | NeverPresentChild of string                                   //child exists expression
+
+
+and LengthType =
+    | Fixed of BigInteger   // this is the case for fixed size sequence/octet strings etc e.g. SEQUENCE (SIZE(10)) OF SOME-TYPE
+    | VarSize               // this is the case for variable size sequence/octet strings etc e.g. SEQUENCE (SIZE(10..20)) OF SOME-TYPE
+
+and Reference =     Reference of string*string
+
+
+and BooleanBinaryOperator =
+    | LogicalOr
+    | LogicalAnd
+
+and ValidatedContraintExpression = 
+    | PrimEqualExpr                             of string*string                        //checked expression it must evaluate to int, char, enum real, value                 -> exp == v in C or exp = v in Ada
+    | StringEqualExpr                           of string*string                        //checked expression, value                 -> strcpm(exp,v) in C or exp = v in Ada
+    | RangeExpr                                 of string*(string*bool)*(string*bool)   //checked expr, lower value, upper value    -> lv <= exp && exp <= uv
+    | LesserThan                                of string*(string*bool)                 //checked expr, value                       -> exp <= v
+    | GreaterThan                               of string*(string*bool)                 //checked expr, value                       -> exp >= v
+    | AlwaysTrue                                
+    | CharInStringExp                           of string*string                        //checked expression, string with valid char set e.g. IA5String(FROM ("ABCDEF"))  
+    | BinaryOpExpr                              of BooleanBinaryOperator*ValidatedContraintExpression*ValidatedContraintExpression     // orperator, exp1, exp2
+    | NotExpr                                   of ValidatedContraintExpression
+
+
+
+type State = {
+        types   : Asn1Type list
+    }
+
+
+let createValidationAst (lang:Ast.ProgrammingLanguage) (app:AstRoot) =
+    GenericFold.foldAstRoot
+        //1. roorFunc r files
+        (fun r files -> files)
+
+        //2. fileFunc r f modules
+        (fun r f modules -> (f.FileNameWithoutExtension, modules) )
+
+        //3. modFunc r f m tases vases
+        (fun r f m tasses vases -> (m.CName, tasses))
+
+        //4. tasFunc r f m tas asn1Type
+        (fun r f m tas t -> {TasValidateFunc.moduleName = m.CName; functionName = tas.c_name; validateType=tas.c_name; typeContent = t})
+
+        //5. vasFunc r f m vas asn1Type asn1Value
+        (fun r f m vas t v -> 0)
+
+        //6. refTypeFunc r f m ref newActType refCons
+        (fun r f m mdName tasName newActType refCons -> RefTypedValidateTypeStatementsBlock {RefTypedValidateTypeStatementsBlock.accessPath=""; refencedTasFunc=(mdName, tasName)})
+
+        //7. baseTypeFunc 
+        (fun r f m t newCons -> BaseTypedValidateTypeStatementBlock {BaseTypedValidateTypeStatementBlock.accessPath=""; returnErrorCode=0; validatedContraintExpressions=newCons})
+
+        //8. enmItemFunc 
+        (fun r f m t newEnmItems newCons -> BaseTypedValidateTypeStatementBlock {BaseTypedValidateTypeStatementBlock.accessPath=""; returnErrorCode=0; validatedContraintExpressions=newCons})
+
+        //9. seqOfTypeFunc 
+        (fun  r f m t  newInnerType newCons -> 
+            SeqOfTypedValidateTypeStatementsBlock
+                {
+                    SeqOfTypedValidateTypeStatementsBlock.accessPath = ""
+                    index      = {LocalVariable.varName = ""; varType = ""}
+                    LengthType  = false
+                    validatedSizeContraintExpressions = newCons
+                    child = newInnerType
+                }
+        )
+
+        //10. seqTypeFunc 
+        (fun  r f m t newChildren newCons   ->
+            SequenceTypedValidateTypeStatementsBlock
+                {
+                    SequenceTypedValidateTypeStatementsBlock.accessPath = ""
+                    contraintExpressions = newCons
+                    children = newChildren
+                }
+        )
+
+        //11. chTypeFunc 
+        (fun r f m t newChildren newCons ->
+            ChoiceTypedValidateTypeStatementsBlock
+                {
+                    ChoiceTypedValidateTypeStatementsBlock.accessPath = ""
+                    contraintExpressions = newCons
+                    children = newChildren
+                }        
+        )
+
+        //12. sequenceChildFunc 
+        (fun  r f m chInfo newType newDefValue ->
+            match chInfo.Optionality with
+            | Some AlwaysAbsent     -> NeverPresentChild ""
+            | Some AlwaysPresent    -> AlwaysPresentChild("", newType)
+            | Some Optional         
+            | Some (Default _)      -> OptionalChild ("", newType)
+            | None                  -> MandatoryChild newType
+        )
+
+        //13. choiceChildFunc 
+        (fun r f m   chInfo newType -> (chInfo.CName_Present lang, newType) )
+
+        //14. refValueFunc  
+        (fun r f m t (md,vs) newActVal -> vs)
+
+        //15. enumValueFunc 
+        (fun r f m t actType enmItem  ni -> enmItem.CEnumName r lang)
+
+        //16. intValFunc 
+        (fun r f m t actType bi  -> bi.ToString())
+
+        //17. realValFunc 
+        (fun r f m t actType d  -> d.ToString())
+
+        //18. ia5StringValFunc
+        (fun r f m t actType s  -> "\"" + s + "\"") 
+        //19. numStringValFunc 
+        (fun r f m t actType s  -> "\"" + s + "\"")
+
+        //20. boolValFunc 
+        (fun r f m t actType b  -> c_var.PrintBooleanValue b)
+
+        //21. octetStringValueFunc 
+        (fun r f m t actType b -> "octec string value")
+
+        //22. bitStringValueFunc 
+        (fun r f m t actType b -> "bit string value")
+
+        //23. nullValueFunc 
+        (fun r f m t actType  -> "0")
+
+        //24. seqOfValueFunc 
+        (fun r f m t actType  newVals -> "sequence of value")
+
+        //25. seqValueFunc 
+        (fun r f m t actType newValues -> "sequence value")
+
+        //26. chValueFunc
+        (fun  r f m t actType name newValue-> "choice value")
+
+        //27. singleValueContraintFunc 
+        (fun r f m t ck actType newValue -> 
+            match t.Kind with
+            | NumericString | IA5String -> StringEqualExpr (actType.AccessPath,newValue)
+            | _                         -> PrimEqualExpr (actType.AccessPath,newValue))
+
+        //28. rangeContraintFunc 
+        (fun r f m t ck actType newValue1 newValue2 b1 b2 -> RangeExpr(actType.AccessPath, (newValue1, b1), (newValue2, b2)))
+
+        //29. greaterThanContraintFunc
+        (fun r f m t ck actType newValue  b -> GreaterThan(actType.AccessPath, (newValue, b)))
+
+        //30. lessThanContraintFunc
+        (fun r f m t ck actType newValue  b -> LesserThan(actType.AccessPath, (newValue, b)))
+
+        //31. alwaysTtrueContraintFunc
+        (fun r f m t checContent actType -> AlwaysTrue)
+
+        //32. typeInclConstraintFunc
+        (fun r f m t actType (md,tas) -> raise(BugErrorException "This constraint should have been removed"))
+
+        //33. unionConstraintFunc
+        (fun r f m t actType nc1 nc2 -> BinaryOpExpr (LogicalAnd, nc1, nc2))
+
+        //34. intersectionConstraintFunc
+        (fun r f m t actType nc1 nc2 -> BinaryOpExpr (LogicalOr, nc1, nc2))
+
+        //35. notConstraintFunc
+        (fun r f m t actType nc      -> NotExpr nc)
+
+        //36. exceptConstraintFunc
+        (fun r f m t actType nc1 nc2 -> BinaryOpExpr (LogicalAnd, nc1, NotExpr nc2))
+
+        //37. rootConstraintFunc
+        (fun r f m t actType nc -> nc)
+
+        //38. root2ConstraintFunc
+        (fun r f m t actType nc1 nc2 -> BinaryOpExpr (LogicalAnd, nc1, nc2))
+
+        app
