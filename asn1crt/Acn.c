fa0ce099c0e1110552b15872bc192add9783e14e
Thanassis Tsiodras <ttsiodras@gmail.com>
2017-05-05 13:44:05 +0200

    Handle embedded platforms that miss strlen via GCC builtin

--- asn1crt/Acn.c
+++ asn1crt/Acn.c
@@ -5,6 +5,11 @@
 
 #include "asn1crt.h"
 
+#if !defined(__unix__) && !defined(_WIN32) && !defined(WIN32)
+// Handle embedded platforms that miss strlen via GCC builtin
+#define strlen __builtin_strlen
+#endif
+
 static byte masks[] = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 };
 
 static flag RequiresReverse(void) 
