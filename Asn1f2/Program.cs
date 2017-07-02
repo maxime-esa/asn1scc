19163dc22e79bb24d175844f712fd7dbf2eaf130
gmamais <gmamais@gmail.com>
2017-07-02 20:00:21 +0300

    resolving bug introduced with latest commit.

--- Asn1f2/Program.cs
+++ Asn1f2/Program.cs
@@ -635,13 +635,13 @@ namespace Asn1f2
 
         public static List<Tuple<ITree, string, IToken[]>> ParseAsn1InputFiles(IEnumerable<string> inputFiles)
         {
-            var groupedByFileName = inputFiles.GroupBy(f => Path.GetFileNameWithoutExtension(f)).ToList();
+            var groupedByFileName = inputFiles.GroupBy(f => Path.GetFileName(f)).ToList();
             return groupedByFileName.Select(f => Parse(f, fs => new asn1Lexer(fs), ts => new asn1Parser(ts), p => (CommonTree)p.moduleDefinitions().Tree)).ToList();
         }
 
         public static List<Tuple<ITree, string, IToken[]>> ParseAcnInputFiles(IEnumerable<string> inputFiles)
         {
-            var groupedByFileName = inputFiles.GroupBy(f => Path.GetFileNameWithoutExtension(f));
+            var groupedByFileName = inputFiles.GroupBy(f => Path.GetFileName(f));
             return groupedByFileName.Select(f => Parse(f, fs => new acnLexer(fs), ts => new acnParser(ts), p => (CommonTree)p.moduleDefinitions().Tree)).ToList();
         }
 
