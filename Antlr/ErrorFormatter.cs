c595110870a848fdb43e9cec1abc93ca38899fd9
Konrad Grochowski <kgrochowski@n7mobile.com>
2017-07-04 15:10:16 +0200

    GCC-style error format ([file]:[line]:[column]: error: [error message])
    This simplifies parsing errors when asn1scc is included in some tool
    chain (like: .asn1 -> asn1scc -> .c -> gcc -> ...)

---
+++ Antlr/ErrorFormatter.cs
@@ -0,0 +1,23 @@
+﻿using Antlr.Runtime;
+
+namespace Antlr
+{
+    public static class ErrorFormatter
+    {
+        private static string FormatErrorHeader(string path, int line, int column)
+        {
+            var arr = new object[] { System.IO.Path.GetFileName(path), ":", line, ":", column, ": error:" };
+            return string.Concat(arr);
+        }
+
+        public static string GetErrorHeader(string path, RecognitionException e)
+        {
+            return FormatErrorHeader(path, e.Line, e.CharPositionInLine);
+        }
+
+        public static string FormatError(string path, int line, int column, string message)
+        {
+            return FormatErrorHeader(path, line, column) + " " + message;
+        }
+    }
+}
