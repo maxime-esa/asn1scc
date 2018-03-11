d2b4cd2ea635199a1d900bcabdbf7a51d09bf5e1
Thanassis Tsiodras <ttsiodras@gmail.com>
2018-03-11 18:10:40 +0100

    The addition of GitVersionTask requires using the distro-provided libgit2-24.

--- circleci-build.sh
+++ circleci-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 nuget restore || exit 1
+cd packages/GitVersionTask.3.6.5/lib/linux/x86_64 || exit 1
+mv libgit2-baa87df.so libgit2-baa87df.so.Windows.people.hardcode.everything
+ln -s $(/bin/ls /usr/lib/x86_64-linux-gnu/libgit2.so.* | sort | head -1) libgit2-baa87df.so  || exit 1
+cd - || exit 1
 xbuild /p:TargetFrameworkVersion="v4.5" || exit 1
 cd v4Tests || exit 1
 make || exit 1
