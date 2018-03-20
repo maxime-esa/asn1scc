740183e12932682b20362c1f429dc53680fa2f7b
gmamais <g_mamais@neuropublic.gr>
2018-03-20 10:34:57 +0200

    Initial design in order to decouple core ASN1SCC modules (FrontEndAst,BackendAst) from language specific macros. This will enable easier introduction of a new language such as Python.

---
+++ CommonTypes/AbstractMacros.fs
@@ -0,0 +1,49 @@
+﻿module AbstractMacros
+open System
+open System.Numerics
+open CommonTypes
+
+    module ACN =
+        type IntegerMacros_positiveInteger_ConstSize_8 = {
+            p:string 
+            soMF:string option 
+            soMFM:string option 
+            nUperMin:BigInteger
+            nUperMax:BigInteger
+            codec:Codec 
+        }
+
+        [<AbstractClass>]
+        type IntegerMacros () =
+            abstract member positiveInteger_ConstSize_8_x : inp:IntegerMacros_positiveInteger_ConstSize_8 -> string;
+            member this.positiveInteger_ConstSize_8 (p:string) (soMF:string option) (soMFM:string option) (nUperMin:BigInteger) (nUperMax:BigInteger) (codec:Codec) : string =
+                this.positiveInteger_ConstSize_8_x ({IntegerMacros_positiveInteger_ConstSize_8.p=p; soMF=soMF; soMFM=soMFM; nUperMin=nUperMin; nUperMax=nUperMax; codec=codec})
+            abstract member positiveInteger_ConstSize_big_endian_16 : p:string -> soMF:string option -> soMFM:string option -> nUperMin:BigInteger -> nUperMax:BigInteger -> codec:Codec -> string;
+
+        type RealMacros =
+            abstract member positiveInteger_ConstSize_8 : p:string -> soMF:string option -> soMFM:string option -> nUperMin:BigInteger-> nUperMax:BigInteger-> codec:Codec -> string;
+            abstract member positiveInteger_ConstSize_big_endian_16 : p:string -> soMF:string option -> soMFM:string option -> nUperMin:BigInteger -> nUperMax:BigInteger -> codec:Codec -> string;
+
+        type Macros = {
+            integerMacros   : IntegerMacros
+            realMacros      : RealMacros
+        }
+
+    module uPER =
+        type IntegerMacros =
+            abstract member positiveInteger_ConstSize_8 : p:string -> soMF:string option -> soMFM:string option -> nUperMin:BigInteger-> nUperMax:BigInteger-> codec:Codec -> string;
+            abstract member positiveInteger_ConstSize_big_endian_16 : p:string -> soMF:string option -> soMFM:string option -> nUperMin:BigInteger -> nUperMax:BigInteger -> codec:Codec -> string;
+
+        type RealMacros =
+            abstract member positiveInteger_ConstSize_8 : p:string -> soMF:string option -> soMFM:string option -> nUperMin:BigInteger-> nUperMax:BigInteger-> codec:Codec -> string;
+            abstract member positiveInteger_ConstSize_big_endian_16 : p:string -> soMF:string option -> soMFM:string option -> nUperMin:BigInteger -> nUperMax:BigInteger -> codec:Codec -> string;
+
+        type Macros = {
+            integerMacros   : IntegerMacros
+            realMacros      : RealMacros
+        }
+
+type LangMacros = {
+    acn     : ACN.Macros
+    uper    : uPER.Macros
+}
