6a3026993f8310aea5be0863e318311b0f96314f
Thanassis Tsiodras <ttsiodras@gmail.com>
2018-10-21 13:19:03 +0200

    Bump version to 4.1f (new release)

--- Asn1f4/Program.fs
+++ Asn1f4/Program.fs
@@ -85,7 +85,7 @@ let printVersion () =
     //let assembly = System.Reflection.Assembly.GetExecutingAssembly();
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
-    let version = "4.0.0.4"
+    let version = "4.1f"
     printfn "asn1scc version %s\n" version
     ()    
 
