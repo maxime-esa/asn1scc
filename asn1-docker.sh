3d60b71c06f34d51a76a5d37f4c4ae675bab4965
Hakan Urhan <hakanurhan@gmail.com>
2019-05-03 15:42:05 +0200

    docker image now can be called as executable for local asn files

---
+++ asn1-docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --rm -v $PWD:/tmp/mounted -w /tmp/mounted -u $(id -u):$(id -g) -e PATH="/opt/asn1scc:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" asn1scc:latest mono /opt/asn1scc/asn1.exe "$@"
