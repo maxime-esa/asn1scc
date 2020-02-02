7a356646aff852fe441c43a9d6fd721083bab378
gmamais <gmamais@gmail.com>
2020-02-02 19:01:06 +0200

    increase version no

--- Asn1f4/Program.fs
+++ Asn1f4/Program.fs
@@ -89,7 +89,7 @@ let printVersion () =
     //let assembly = System.Reflection.Assembly.GetExecutingAssembly();
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
-    let version = "4.2.1.2f"
+    let version = "4.2.1.3f"
     printfn "asn1scc version %s\n" version
     ()    
 
