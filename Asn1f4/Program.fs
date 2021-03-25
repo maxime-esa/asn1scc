e2006e1fed5630dc2fef72972c11ce04adb18ecf
Thanassis Tsiodras <ttsiodras@gmail.com>
2021-03-25 15:36:14 +0100

    Version bump.

--- Asn1f4/Program.fs
+++ Asn1f4/Program.fs
@@ -95,7 +95,7 @@ let printVersion () =
     //let assembly = System.Reflection.Assembly.GetExecutingAssembly();
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
-    let version = "4.2.4.4f"
+    let version = "4.2.4.5f"
     printfn "asn1scc version %s\n" version
     ()
 
