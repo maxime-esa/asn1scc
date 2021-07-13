710f776a15b2d21504af3695b54a574e2fd369ce
gmamais <g_mamais@neuropublic.gr>
2021-07-13 16:36:20 +0300

    New Project asn1sccWs : New project that exposes the functionality of asn1scc as web service. To be used by the LSP

---
+++ Asn1sccWs/Startup.fs
@@ -0,0 +1,32 @@
+namespace Asn1sccWs
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy;
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+type Startup(configuration: IConfiguration) =
+    member _.Configuration = configuration
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    member _.ConfigureServices(services: IServiceCollection) =
+        // Add framework services.
+        services.AddControllers() |> ignore
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    member _.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
+        if (env.IsDevelopment()) then
+            app.UseDeveloperExceptionPage() |> ignore
+        app.UseHttpsRedirection()
+           .UseRouting()
+           .UseAuthorization()
+           .UseEndpoints(fun endpoints ->
+                endpoints.MapControllers() |> ignore
+            ) |> ignore
