784651454588d4a5ee0b098af9c279fd31fc0942
Thanassis Tsiodras <ttsiodras@gmail.com>
2021-10-25 14:13:38 +0200

    Bump the version up to 4.2.4.8

--- asn1scc/Program.fs
+++ asn1scc/Program.fs
@@ -95,7 +95,7 @@ let printVersion () =
     //let assembly = System.Reflection.Assembly.GetExecutingAssembly();
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
-    let version = "4.2.4.7f"
+    let version = "4.2.4.8f"
     printfn "asn1scc version %s\n" version
     ()
 
