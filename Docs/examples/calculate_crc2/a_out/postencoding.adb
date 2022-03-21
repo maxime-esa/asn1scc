e943157514cc56636837b84f6224363c5dfd466c
Maxime Perrotin <maxime.perrotin@esa.int>
2022-03-21 10:29:45 +0100

    Reformat Ada code

--- Docs/examples/calculate_crc2/a_out/postencoding.adb
+++ Docs/examples/calculate_crc2/a_out/postencoding.adb
@@ -1,45 +1,48 @@
+with adaasn1rtl.encoding.acn;
+with Interfaces;
+use Interfaces;
 package body postencoding with
    SPARK_Mode
 is
 
-   --dummy function that (supposedly) calculates the crc of a buffer
-   function calc_crc_32 (buf : OctetBuffer; len1 : Natural) return Integer is
-   begin
-      return 0;
-   end calc_crc_32;
+   --  dummy function that (supposedly) calculates the crc of a buffer
+   function calc_crc_32 (unused_buf : OctetBuffer; unused_len1 : Natural)
+       return Integer is (0);
 
    procedure my_encoding_patcher
-     (val                       : in     Packet;
-      bitStreamPositions_start1 :        adaasn1rtl.encoding.BitstreamPtr;
+     (unused_val                :        Packet;
+      unused_bitStreamPositions_start1 : adaasn1rtl.encoding.BitstreamPtr;
       bitStreamPositions_1      :        Packet_extension_function_positions;
       bs                        : in out adaasn1rtl.encoding.Bitstream)
    is
-      startPosInBits : adaasn1rtl.Asn1UInt :=
+      startPosInBits : constant adaasn1rtl.Asn1UInt :=
         adaasn1rtl.Asn1UInt
           (bitStreamPositions_1.Packet_body_length_in_bytes.Current_Bit_Pos);
-      endPosIBits : adaasn1rtl.Asn1UInt :=
+      endPosIBits : constant adaasn1rtl.Asn1UInt :=
         adaasn1rtl.Asn1UInt
           (bitStreamPositions_1.Packet_packet_crc32.Current_Bit_Pos);
-      lengthInBytes : adaasn1rtl.Asn1UInt :=
+      lengthInBytes : constant adaasn1rtl.Asn1UInt :=
         (endPosIBits - startPosInBits - 16) / 8;
       crc32          : adaasn1rtl.Asn1UInt;
       orignalBit_Pos : constant Natural := bs.Current_Bit_Pos;
    begin
 
-      --use the ACN function to encode the length value. Please note that we use the Packet_packet_length_in_bytes field in the
-      -- Packet_extension_function_positions to encode the length in the correct position.
+      --  use the ACN function to encode the length value. Please note that
+      --   we use the Packet_packet_length_in_bytes field in the
+      --  Packet_extension_function_positions to encode the length in the
+      --  correct position.
       bs.Current_Bit_Pos :=
         bitStreamPositions_1.Packet_body_length_in_bytes.Current_Bit_Pos;
       adaasn1rtl.encoding.acn.Acn_Enc_Int_PositiveInteger_ConstSize
         (bs, lengthInBytes, 16);
 
-      --calculate crc
+      --  calculate crc
       crc32 :=
         adaasn1rtl.Asn1UInt
           (calc_crc_32
              (bs.Buffer,
               bitStreamPositions_1.Packet_packet_crc32.Current_Bit_Pos / 8));
-      --encode crc32 in the correct position
+      --  encode crc32 in the correct position
       bs.Current_Bit_Pos :=
         bitStreamPositions_1.Packet_packet_crc32.Current_Bit_Pos;
       adaasn1rtl.encoding.acn.Acn_Enc_Int_PositiveInteger_ConstSize
@@ -49,41 +52,43 @@ is
    end my_encoding_patcher;
 
    function crc_validator
-     (val                       : in     Packet;
-      bitStreamPositions_start1 :        adaasn1rtl.encoding.BitstreamPtr;
+     (unused_val                    :        Packet;
+      unused_bitStreamPositions_start1 : adaasn1rtl.encoding.BitstreamPtr;
       bitStreamPositions_1      :        Packet_extension_function_positions;
       bs : in out adaasn1rtl.encoding.Bitstream) return adaasn1rtl.ASN1_RESULT
    is
-      startPosInBits : adaasn1rtl.Asn1UInt :=
+      startPosInBits : constant adaasn1rtl.Asn1UInt :=
         adaasn1rtl.Asn1UInt
           (bitStreamPositions_1.Packet_body_length_in_bytes.Current_Bit_Pos);
-      endPosIBits : adaasn1rtl.Asn1UInt :=
+      endPosIBits : constant adaasn1rtl.Asn1UInt :=
         adaasn1rtl.Asn1UInt
           (bitStreamPositions_1.Packet_packet_crc32.Current_Bit_Pos);
-      lengthInBytes : adaasn1rtl.Asn1UInt :=
+      lengthInBytes : constant adaasn1rtl.Asn1UInt :=
         (endPosIBits - startPosInBits - 16) / 8;
       decLenInBytes  : adaasn1rtl.Asn1UInt := 0;
       crc32          : adaasn1rtl.Asn1UInt;
       decodeCrc32    : adaasn1rtl.Asn1UInt;
-      orignalBit_Pos : constant Natural    := bs.Current_Bit_Pos;
+      --  orignalBit_Pos : constant Natural    := bs.Current_Bit_Pos;
       ret1           : adaasn1rtl.ASN1_RESULT;
       ret2           : adaasn1rtl.ASN1_RESULT;
       ret            : adaasn1rtl.ASN1_RESULT;
    begin
-      --use the ACN function to decode the length value. Please note that we use the Packet_packet_length_in_bytes field in the
-      -- Packet_extension_function_positions to encode the length in the correct position.
+      --  use the ACN function to decode the length value.
+      --  Please note that we use the Packet_packet_length_in_bytes field
+      --  in the Packet_extension_function_positions to encode the length
+      --  in the correct position.
       bs.Current_Bit_Pos :=
         bitStreamPositions_1.Packet_body_length_in_bytes.Current_Bit_Pos;
       adaasn1rtl.encoding.acn.Acn_Dec_Int_PositiveInteger_ConstSize
         (bs, decLenInBytes, 0, 65535, 16, ret1);
 
-      --calculate crc
+      --  calculate crc
       crc32 :=
         adaasn1rtl.Asn1UInt
           (calc_crc_32
              (bs.Buffer,
               bitStreamPositions_1.Packet_packet_crc32.Current_Bit_Pos / 8));
-      --decode crc32 from the bitstream
+      --  decode crc32 from the bitstream
       bs.Current_Bit_Pos :=
         bitStreamPositions_1.Packet_packet_crc32.Current_Bit_Pos;
       adaasn1rtl.encoding.acn.Acn_Dec_Int_PositiveInteger_ConstSize
@@ -96,7 +101,7 @@ is
              lengthInBytes = decLenInBytes and crc32 = decodeCrc32,
            ErrorCode => 0);
       if not ret.Success then
-         ret.ErrorCode := 3141592; -- assign custom error code
+         ret.ErrorCode := 3141592;  --  assign custom error code
       end if;
 
       return ret;
--- Docs/examples/calculate_crc2/a_out/postencoding.ads
+++ Docs/examples/calculate_crc2/a_out/postencoding.ads
@@ -1,31 +1,23 @@
 with adaasn1rtl;
-with System;
 use adaasn1rtl;
 with adaasn1rtl.encoding;
-with adaasn1rtl.encoding.acn;
 with MYMODULE;
 
-use type adaasn1rtl.OctetBuffer;
-use type adaasn1rtl.BitArray;
-use type adaasn1rtl.Asn1UInt;
-use type adaasn1rtl.Asn1Int;
-use type adaasn1rtl.BIT;
 use MYMODULE;
---# inherit ;
 
 package postencoding with
    SPARK_Mode
 is
 
    procedure my_encoding_patcher
-     (val                       : in     Packet;
-      bitStreamPositions_start1 :        adaasn1rtl.encoding.BitstreamPtr;
+     (unused_val                       : Packet;
+      unused_bitStreamPositions_start1 : adaasn1rtl.encoding.BitstreamPtr;
       bitStreamPositions_1      :        Packet_extension_function_positions;
       bs                        : in out adaasn1rtl.encoding.Bitstream);
 
    function crc_validator
-     (val                       : in     Packet;
-      bitStreamPositions_start1 :        adaasn1rtl.encoding.BitstreamPtr;
+     (unused_val                       :        Packet;
+      unused_bitStreamPositions_start1 :  adaasn1rtl.encoding.BitstreamPtr;
       bitStreamPositions_1      :        Packet_extension_function_positions;
       bs : in out adaasn1rtl.encoding.Bitstream) return adaasn1rtl.ASN1_RESULT;
 
