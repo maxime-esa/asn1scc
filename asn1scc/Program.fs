ddeae81a74c2a49be66fe4dcb95c5f10033c453e
Maxime Perrotin <Maxime.Perrotin@esa.int>
2022-11-07 20:30:43 +0100

    Update version to 4.4.0.1

--- asn1scc/Program.fs
+++ asn1scc/Program.fs
@@ -101,7 +101,7 @@ let printVersion () =
     //let assembly = System.Reflection.Assembly.GetExecutingAssembly();
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
-    let version = "4.4.0.0"
+    let version = "4.4.0.1"
     printfn "asn1scc version %s\n" version
     ()
 
