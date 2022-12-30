8e38f6b4130932cfea572fd064cb83125a85ac02
usr3-1415 <11031787+usr3-1415@users.noreply.github.com>
2022-12-30 14:22:46 +0200

    back to large ... Trying Ada along with C

--- circleci-build.sh
+++ circleci-build.sh
@@ -4,3 +4,4 @@ dotnet build parseStg2/
 dotnet build "asn1scc.sln"
 cd v4Tests || exit 1
 ../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 16
+../regression/bin/Debug/net7.0/regression -l Ada -ws 8 -s true -p 16
