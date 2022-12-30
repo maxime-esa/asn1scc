1b7293b5631e0d3169e854762288ba03fbc83283
usr3-1415 <11031787+usr3-1415@users.noreply.github.com>
2022-12-30 15:42:39 +0200

    32 bit test cases

--- circleci-build.sh
+++ circleci-build.sh
@@ -3,5 +3,5 @@ dotnet build Antlr/
 dotnet build parseStg2/
 dotnet build "asn1scc.sln"
 cd v4Tests || exit 1
-../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 16
-../regression/bin/Debug/net7.0/regression -l Ada -ws 8 -s true -p 16
+../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 16 || exit 1
+../regression/bin/Debug/net7.0/regression -l Ada -ws 8 -s true -p 16 || exit 1
