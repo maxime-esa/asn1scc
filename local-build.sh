cb1a7046d6a358c2ba125fda9397e40651d24c7f
usr3-1415 <11031787+usr3-1415@users.noreply.github.com>
2023-01-01 01:21:01 +0200

    Thread pool size set to 48

--- local-build.sh
+++ local-build.sh
@@ -7,9 +7,9 @@ dotnet build Antlr/
 dotnet build parseStg2/
 dotnet build "asn1scc.sln"
 cd v4Tests || exit 1
-../regression/bin/Debug/net7.0/regression -l c -ws 4 -s false -p 32 || exit 1
-../regression/bin/Debug/net7.0/regression -l Ada -ws 4 -s false -p 32 || exit 1
-../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 32 || exit 1
-../regression/bin/Debug/net7.0/regression -l Ada -ws 8 -s true -p 24 || exit 1
+../regression/bin/Debug/net7.0/regression -l c -ws 4 -s false -p 48 || exit 1
+../regression/bin/Debug/net7.0/regression -l Ada -ws 4 -s false -p 48 || exit 1
+../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 48 || exit 1
+../regression/bin/Debug/net7.0/regression -l Ada -ws 8 -s true -p 48 || exit 1
 
 
