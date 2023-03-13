5633bf86b229203847563529391a5dcd29e25763
usr3-1415 <11031787+usr3-1415@users.noreply.github.com>
2023-03-13 17:47:35 +0200

    fix bug in instantiating enumerated types defined in a seperate module.

--- asn1scc/Program.fs
+++ asn1scc/Program.fs
@@ -102,7 +102,7 @@ let printVersion () =
     //let fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
     //let version = fvi.FileVersion;
 
-    let version = "4.5.0.2"
+    let version = "4.5.0.3"
     printfn "asn1scc version %s\n" version
     ()
 
