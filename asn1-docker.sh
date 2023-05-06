ccf55b841fee0741ca6a6d7fbbbcd46841742928
usr3-1415 <11031787+usr3-1415@users.noreply.github.com>
2023-05-06 12:25:41 +0300

    Problem running ./asn1-docker.sh #261

--- asn1-docker.sh
+++ asn1-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v $PWD:/tmp/mounted -w /tmp/mounted -u $(id -u):$(id -g) -e PATH="/opt/asn1scc:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" asn1scc-runtime /opt/asn1scc/asn1scc "$@"
+docker run --rm -v $PWD:/tmp/mounted -w /tmp/mounted -u $(id -u):$(id -g) -e PATH="/opt/asn1scc:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" asn1scc-runtime "$@"
