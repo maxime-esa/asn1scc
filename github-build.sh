0b7c15c2d4367707b100e758bc8f7c1383f6aabf
usr3-1415 <11031787+usr3-1415@users.noreply.github.com>
2024-02-24 14:16:02 +0200

    decreate the thread pool size in regression when running in github due to limited resources

--- github-build.sh
+++ github-build.sh
@@ -3,10 +3,10 @@ dotnet build Antlr/
 dotnet build parseStg2/
 dotnet build "asn1scc.sln"
 cd v4Tests || exit 1
-../regression/bin/Debug/net7.0/regression -l c -ws 4 -s false -p 48 || exit 1
-../regression/bin/Debug/net7.0/regression -l Ada -ws 4 -s false -p 48 || exit 1
-../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 48 -ig || exit 1
-../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 48 || exit 1
-../regression/bin/Debug/net7.0/regression -l Ada -ws 8 -s true -p 48 || exit 1
+../regression/bin/Debug/net7.0/regression -l c -ws 4 -s false -p 12 || exit 1
+../regression/bin/Debug/net7.0/regression -l Ada -ws 4 -s false -p 12 || exit 1
+../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 12 -ig || exit 1
+../regression/bin/Debug/net7.0/regression -l c -ws 8 -s true -p 12 || exit 1
+../regression/bin/Debug/net7.0/regression -l Ada -ws 8 -s true -p 12 || exit 1
 
 
