457645aa3cf8833c8c45e5315ae10aa66d6e9c72
Samuel Chassot <samuel.chassot@epfl.ch>
2024-06-21 14:36:45 +0200

    add scripts to verify and run genc

---
+++ asn1scala/bitStream_genc.sh
@@ -0,0 +1,8 @@
+stainless-dotty \
+src/main/scala/asn1scala/asn1jvm.scala  \
+src/main/scala/asn1scala/asn1jvm_Verification.scala  \
+src/main/scala/asn1scala/asn1jvm_Helper.scala \
+src/main/scala/asn1scala/asn1jvm_Bitstream.scala \
+--config-file=stainless.conf \
+--genc=true\
+$1
